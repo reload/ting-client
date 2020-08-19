27dce51a6914594b4ae958da91c24004497f1138
Thomas Fini Hansen <xen@xen.dk>
2020-08-19 08:35:06 +0200

    Fix watchdog logging
    When logging with watchdog, the message is assumed to be a
    translatable string. But debugging produces messages too big for the
    translation system to handle.
    
    This changes the message to use a placeholder and add the debug
    message as a variable.

--- lib/log/TingClientDrupalWatchDogLogger.php
+++ lib/log/TingClientDrupalWatchDogLogger.php
@@ -6,10 +6,23 @@
  * @see http://api.drupal.org/api/function/watchdog/
  */
 class TingClientDrupalWatchDogLogger extends TingClientLogger {
-  public function doLog($message, $severity) {
-    watchdog('ting client', htmlspecialchars($message, ENT_QUOTES, 'UTF-8', FALSE), array(),
-             constant('WATCHDOG_' . $severity),
-             'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-  }
-}
 
+  /**
+   * Log a message to the Drupal watchdog.
+   *
+   * @param string $message
+   *   Message to log.
+   * @param string $severity
+   *   Severity to log with.
+   */
+  public function doLog($message, $severity) {
+    watchdog(
+      'ting client',
+      '<pre>@message</pre>',
+      ['@message' => $message],
+      constant('WATCHDOG_' . $severity),
+      'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]
+    );
+  }
+
+}
