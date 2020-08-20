d8797b83a15be36e567ca9b38775240c5bf95fc3
Thomas Fini Hansen <xen@xen.dk>
2020-08-20 10:00:40 +0200

    1519: Categorize errors
    To facilitate custom error messages.

--- lib/exception/TingClientException.php
+++ lib/exception/TingClientException.php
@@ -1,20 +1,65 @@
 <?php
 
+/**
+ * Class for TingClient errors.
+ */
 class TingClientException extends Exception {
-  public $user_message = NULL;
-  private $errors = array(
-    'Unsupported index',
-    'Unsupported boolean modifier',
-    'Invalid or unsupported use',
-    'Internal problem',
+  /**
+   * Type for errors not explicitly matched.
+   */
+  const TYPE_UNKNOWN_ERROR = 0;
+
+  /**
+   * Query syntax error.
+   */
+  const TYPE_SYNTAX_ERROR = 1;
+
+  /**
+   * Internal errors.
+   */
+  const TYPE_INTERNAL_ERROR = 2;
+
+  /**
+   * Type of error.
+   *
+   * @var int
+   */
+  protected $type = NULL;
+
+  /**
+   * Maps error messages to error types.
+   *
+   * @var array
+   */
+  protected $errorMapping = array(
+    'Query syntax error' => self::TYPE_SYNTAX_ERROR,
+    'Unsupported index' => self::TYPE_SYNTAX_ERROR,
+    'Unsupported boolean modifier' => self::TYPE_SYNTAX_ERROR,
+    'Invalid or unsupported use' => self::TYPE_SYNTAX_ERROR,
+    'Internal problem' => self::TYPE_INTERNAL_ERROR,
   );
 
-  public function __construct($message, $code = 0, Exception $previous = null) {
-    foreach($this->errors as $val){
-      if(strpos($message, $val) !== FALSE){
-        $this->user_message = $val;
+  /**
+   * Get the type of error.
+   *
+   * Makes it possible to differentiate between syntax, internal and other
+   * errors.
+   *
+   * @return int
+   *   The type of error.
+   */
+  public function getType() {
+    if (is_null($this->type)) {
+      foreach ($this->errorMapping as $string => $type) {
+        if (strpos($this->getMessage(), $string) !== FALSE) {
+          $this->type = $type;
+
+          break;
+        }
       }
+
+      return $this->type;
     }
-    parent::__construct($message, $code, $previous);
   }
+
 }
